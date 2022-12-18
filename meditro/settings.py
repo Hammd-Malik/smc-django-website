d0333f0639dace588f3facab8ff4ff00f85d1024
Hammad Malik <hammadmalik5353@gmail.com>
2022-12-18 13:24:49 +0500

    updated

--- meditro/settings.py
+++ meditro/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
