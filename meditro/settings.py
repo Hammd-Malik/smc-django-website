9b58e346367f78cd1c4465ae1b00750590f3f285
Hammad Malik <hammadmalik5353@gmail.com>
2022-12-20 23:04:10 +0500

    updated

--- meditro/settings.py
+++ meditro/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic', 
+    'corsheaders',
     'website',
     'adminDashboard'
 ]
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'meditro.urls'
